862d7d38179def479b2f6b7432b9508b43a716d6
Pallavi G D <gdpallavi370@gmail.com>
2026-01-30 01:05:02 +0530

    Fix ESLint image alt error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
 import Movie from "./components/Movie";
